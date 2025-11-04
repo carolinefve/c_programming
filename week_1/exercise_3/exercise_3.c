5266bd12ebbf567655819b7fefac0fff51147f0f
carolinefve <cveloso0808@gmail.com>
2025-11-04 22:37:49 +0000

    Add exercise 3

---
+++ week_1/exercise_3/exercise_3.c
@@ -0,0 +1,17 @@
+// This program displays an heptagon
+
+#include "graphics.h"
+
+int main(void)
+{
+    drawLine(85,35,135,60);
+    drawLine(135,60,145,110);
+    drawLine(145,110,110,145);
+    drawLine(110,145,60,145);
+    drawLine(60,145,25,110);
+    drawLine(25,110,35,60);
+    drawLine(35,60,85,35);
+
+   
+    return 0;
+}
